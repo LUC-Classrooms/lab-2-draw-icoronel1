869515e75b9eb7171afd8ed6c122ffd4320462ae
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-01-26 20:21:26 +0000

    Created Jiggly Puff

--- script.js
+++ script.js
@@ -5,23 +5,33 @@ function setup(){
 function draw(){
   background(200);
   // add your drawing code here ...
-  fill(200, 15, 200)
-  triangle(width/2 - 45, height/2 - 55, width/2 - 25, height/2 - 40, width/2 - 45, height/2 - 20)
-  triangle(width/2 + 45, height/2 - 55, width/2 + 25, height/2 - 40, width/2 + 45, height/2 - 20)
-  ellipse(width/2, height/2, 100) //head
-  fill("yellow")
-  ellipse(width/2 - 20, height/2 -10, 10) //left eye
-  ellipse(width/2 + 20, height/2 -10, 10) //right eye
   fill("pink")
-  triangle(width/2 - 5, height/2, width/2 + 5, height/2, width/2, height/2 + 8) //nose
+  //ellipse(width/2, height/2, 200) //head-body
+triangle(width/2 - 85, height/2 - 125, width/2 - 30, height/2 - 95, width/2 - 85, height/2 - 60) // left outer ear
+triangle(width/2 + 95, height/2 - 115, width/2 + 40, height/2 - 80, width/2 + 95, height/2 - 35) // right outer ear
+  ellipse(width/2 - 50, height/2 + 100, 45, 20)
+  ellipse(width/2 + 50, height/2 + 100, 45, 20)
+  fill("brown")
+  triangle(width/2 - 82, height/2 - 115, width/2 - 40, height/2 - 85, width/2 - 75, height/2 - 50) // left inner ear
+  triangle(width/2 + 90, height/2 - 105, width/2 + 50, height/2 - 70, width/2 + 85, height/2 - 35) // right inner ear
+  fill("pink")
+  ellipse(width/2, height/2, 210) //head-body
+  fill("white")
   strokeWeight(2)
-  line(width/2 - 10, height/2 + 5, width/2 - 35, height/2) //left top whisker
-  line(width/2 + 10, height/2 + 5, width/2 + 35, height/2) //right top whisker
-  line(width/2 - 10, height/2 + 5, width/2 - 35, height/2 + 10) //middle left whisker
-  line(width/2 + 10, height/2 + 5, width/2 + 35, height/2 + 10) //middle right whisker
-  line(width/2 - 10, height/2 + 5, width/2 - 35, height/2 + 20) //bottom left whisker
-  line(width/2 + 10, height/2 + 5, width/2 + 35, height/2 + 20) //bottom right whisker
-  noFill();
-  arc(width/2 - 10, height/2 + 12, 20, 20, 0, PI)
-  arc(width/2 + 10, height/2 + 12, 20, 20, 0, PI)
+  ellipse(width/2 - 50, height/2 - 20, 70) //left eye
+  ellipse(width/2 + 50, height/2 + 5, 70) //right eye
+ fill("teal")
+  ellipse(width/2 - 40, height/2 - 15, 50) // left eye pupil
+  ellipse(width/2 + 60, height/2 + 10, 50) // right eye pupil
+  fill("white")
+  ellipse(width/2 - 27, height/2 - 30, 10) // left eye highlight
+  ellipse(width/2 + 70, height/2 - 8, 10) // right eye highlight
+  noFill()
+  arc(width/2, height/2, 10, 10, 0, PI) //mouth
+  arc(width/2 - 75, height/2 + 30, 25, 55, 0, PI) // L arm
+  arc(width/2 + 65, height/2 + 50, 25, 55, 0, PI) // R arm 
+  fill("pink")
+  arc(width/2 + 10, height/2 - 95, 85, 75, PI, TWO_PI+HALF_PI)
+  noFill()
+  arc(width/2 + 10, height/2 - 70, 65, 65, 0, PI + HALF_PI)
 }
